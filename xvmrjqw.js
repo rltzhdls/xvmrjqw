8454f41c95bc0b3efa237b622ac0dc28e55fabd4
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2025-02-21 22:38:43 +0900

    Add files via upload

---
+++ xvmrjqw.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    const fleftDiv = document.querySelector('.lefts');
+        if (fleftDiv) {
+            if (!fleftDiv.innerHTML.includes('Sui Design')) {
+                const pElement = document.createElement('p');
+                pElement.textContent = 'Copyright Sui Design';
+                fleftDiv.appendChild(pElement);
+            }
+        } else {
+            window.location.href = 'https://suiblog.site';
+        }
+});
+
